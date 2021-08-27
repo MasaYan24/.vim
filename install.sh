74e9438cd8f6f454273ac2d91b12a6ca6ae23a26
2386995 <masahiro.takahashi.j7x@jp.denso.com>
2021-08-27 15:25:05 +0000

    update

--- install.sh
+++ install.sh
@@ -2,3 +2,5 @@ ln -s $HOME/.vim/vimrc.vim $HOME/.vimrc
 
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" -- $HOME/.vim/bundles/dein
