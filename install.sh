6f504cf412a8c7953f0f2c3f16bca0c171ac8678
takahashi <7567050+MasaYan24@users.noreply.github.com>
2021-09-02 23:30:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,5 @@ mkdir -p $HOME/.config/nvim
 ln -s $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
 
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" -- $HOME/.vim/bundles/dein
+
+pip install pynvim
