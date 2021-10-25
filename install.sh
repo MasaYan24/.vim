9a02c62b07410fb639fa7bdc586aaf8825a561c6
takahashi <7567050+MasaYan24@users.noreply.github.com>
2021-10-25 23:10:09 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,4 +8,4 @@ ln -s $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" \
   -- $HOME/.vim/bundles/dein
 
-pip install pynvim
+pip install autopep8 black flake8 isort mypy pynvim
