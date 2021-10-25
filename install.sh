be6fe971effc0499631f6c4704ffff60aafbf68f
takahashi <7567050+MasaYan24@users.noreply.github.com>
2021-10-25 23:21:27 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 mkdir -p $HOME/.config/nvim
 ln -s $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
 
+sudo apt install -y neovim
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh)" \
   -- $HOME/.vim/bundles/dein
 
