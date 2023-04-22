2b552daef03042ef373b7fbb52dd8024e83e169a
takahashi <7567050+MasaYan24@users.noreply.github.com>
2023-04-23 05:50:07 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-installer=$(mktemp -d)/install_dein.sh
+installer=$(mktemp -d)/dein_install.sh
 wget https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh -O $installer
 sh $installer --use-neovim-config $HOME/.local/share/dein
 
 ln -sf $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
 
-pip install autopep8 black flake8 isort mypy pynvim
+$HOME/.miniconda/bin/pip install autopep8 black flake8 isort mypy pynvim
