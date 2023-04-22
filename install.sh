2b4745beae1212523be619f4b9fa011dff89af72
takahashi <7567050+MasaYan24@users.noreply.github.com>
2023-04-22 20:32:41 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-wget https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh -O dein_installer.sh
-sh dein_installer.sh --use-neovim-config $HOME/.local/share/dein
+installer=$(mktemp).sh
+wget https://raw.githubusercontent.com/Shougo/dein-installer.vim/master/installer.sh -O $installer
+sh $installer --use-neovim-config $HOME/.local/share/dein
 
 ln -sf $HOME/.vim/vimrc.vim $HOME/.config/nvim/init.vim
 
